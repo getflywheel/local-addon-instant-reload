73a6958e49511bf451c5b5a64290f756965078c9
veryspry <ehlinger.matt@gmail.com>
2021-01-05 14:45:02 -0600

    wip: port main thread code from Local

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,51 @@
-// https://getflywheel.github.io/local-addon-api/modules/_local_main_.html
-import * as LocalMain from '@getflywheel/local/main';
+import * as Local from '@getflywheel/local';
+import { getServiceContainer, HooksMain } from '@getflywheel/local/main';
+import InstantReload from './main/InstantReload';
+import { IPC_EVENTS } from './constants';
 
-export default function (context) {
+const serviceContainer = getServiceContainer().cradle;
+
+export default function (context: typeof serviceContainer.addonLoader.addonContext): void {
 	const { electron } = context;
 	const { ipcMain } = electron;
+	const instantReload = new InstantReload();
 
-	LocalMain.addIpcAsyncListener('get-random-count', async () => {
-		return Math.floor(Math.random() * 100);
+	/**
+	 * start site
+	 * if IR not enabled
+	 * 		break
+	 * else
+	 * 		start browsersync
+	 * 		restart the local router (this will be handled by the start method of Local)
+	 * 		start up the file watchers
+	 */
+
+	HooksMain.addAction('siteStarted', (site: Local.Site) => {
+		/**
+		 * Here we need to fire up browser sync, rewrite the local router proxy_pass and
+		 * live links start port
+		 */
+
+		if (site.autoEnableInstantReload) {
+			instantReload.createNewConnection(site);
+			/**
+			 * @todo investigate adding file watchers after the Local router restarts
+			 */
+			instantReload.addFileWatchers(site);
+		}
 	});
 
-	ipcMain.on('save-count', async (event, siteId, count) => {
-		LocalMain.sendIPCEvent('instructions');
-		LocalMain.getServiceContainer().cradle.localLogger.log('info', `Saving count ${count} for site ${siteId}.`);
-		LocalMain.SiteData.updateSite(siteId, {
-			id: siteId,
-			count,
+	HooksMain.addAction('siteStopped', (site: Local.Site) => {
+		instantReload.stopConnection(site);
+	});
+
+	const toggleInstantReloadFactory = (autoEnableInstantReload: boolean) => (_, siteID: string) => {
+		serviceContainer.siteData.updateSite(siteID, {
+			autoEnableInstantReload,
 		});
-	});
+	};
+
+	ipcMain.on(IPC_EVENTS.ENABLE_INSTANT_RELOAD, toggleInstantReloadFactory(true));
+
+	ipcMain.on(IPC_EVENTS.DISABLE_INSTANT_RELOAD, toggleInstantReloadFactory(false));
 }
